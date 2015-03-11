b320f3e344e54c0caa1537abf6c8a0e54b5dfaa6
sagar rabadiya <sam.coolone70@gmail.com>
2015-03-11 11:27:46 +0530

    inspinia layout added

---
+++ resources/views/layouts/inspinia/inspinia.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>INSPINIA | Dashboard v.2</title>
+
+    <link href="{{asset('inspinia/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('inspinia/font-awesome/css/font-awesome.css')}}" rel="stylesheet">
+
+
+
+    <link href="{{asset('inspinia/css/animate.css')}}" rel="stylesheet">
+    <link href="{{asset('inspinia/css/style.css')}}" rel="stylesheet">
+    <script src="{{asset('inspinia/js/jquery-2.1.1.js')}}"></script>
+</head>
+
+<body>
+<div id="wrapper">
+    @include('layouts.inspinia.inspinia_nav')
+
+    <div id="page-wrapper" class="gray-bg">
+        @include('layouts.inspinia.inspinia_top')
+        <div class="wrapper wrapper-content">
+            @yield('content')
+        </div>
+        <div class="footer">
+            <div>
+                @2015 TribalSquare All rights reserved.
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Mainly scripts -->
+
+<script src="{{asset('inspinia/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('inspinia/js/plugins/metisMenu/jquery.metisMenu.js')}}"></script>
+
+<!-- Custom and plugin javascript -->
+<script src="{{asset('inspinia/js/inspinia.js')}}"></script>
+
+
+<!-- jQuery UI -->
+<script src="{{asset('inspinia/js/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+
+</body>
+</html>
