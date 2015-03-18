6b4752cfa7dfc18881ea5c097048c5bb290d01de
Bhaumik <bhaumikwork@gmail.com>
2015-03-18 18:27:27 +0530

    removed widget box close link

--- resources/views/providers/posting/index.blade.php
+++ resources/views/providers/posting/index.blade.php
@@ -8,14 +8,14 @@
             <div class="ibox float-e-margins">
                 <div class="ibox-title">
                     <h5>My Posting</h5>
-                    <div class="ibox-tools">
+                    <!-- <div class="ibox-tools">
                         <a class="collapse-link">
                             <i class="fa fa-chevron-up"></i>
                         </a>
                         <a class="close-link">
                             <i class="fa fa-times"></i>
                         </a>
-                    </div>
+                    </div> -->
                 </div>                             
                 <div class="ibox-content">                   
                 <div class="col-sm-12">
