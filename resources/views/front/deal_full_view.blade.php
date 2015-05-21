98b51f57c41489291eb59da77569fe3e3d9ce673
Bhaumik <bhaumikwork@gmail.com>
2015-05-21 15:56:44 +0530

    Change in deal full view - 1

--- resources/views/front/deal_full_view.blade.php
+++ resources/views/front/deal_full_view.blade.php
@@ -124,8 +124,8 @@
                                             </div>
                                         </div>
                                         <div class="form-group">
-                                            <label class="col-sm-6 col-md-6 control-label" style="text-align: left;">Refferel code</label>
-                                            <div class="col-sm-6 col-md-6">
+                                            <label class="col-sm-5 col-md-5 control-label" style="text-align: left;">Refferel code</label>
+                                            <div class="col-sm-7 col-md-7">
                                                 <input type="text" name="refferel_code" id="refferel_code" class="form-control required">
                                             </div>
                                             <div class="col-sm-12 error-refferel" style="display: none;color:#ff0000;">
