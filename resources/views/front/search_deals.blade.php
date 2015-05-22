595fb837f67d63d8909365a4992c24859f06072d
bhaumikwork <bhaumikwork@gmail.com>
2015-05-22 14:40:23 +0530

    search deal mobile view link issue -resolve

--- resources/views/front/search_deals.blade.php
+++ resources/views/front/search_deals.blade.php
@@ -63,13 +63,13 @@
                             <h1>New Deals</h1>
 
                             <div class="col-sm-12 all_deal_box_wrap" style="margin-top:10px;">
-                                <div class="row">
+                                <div class="row remove_clearfix">
                                     <div class="col-sm-12"></div>
                                 </div>
                                 <?php $counter = 0; ?>
                                 @foreach( $oDeals AS $oDeal )
-                                    <!-- @if( $counter++ == 0 ) <?php //continue; ?> @endif -->
-                                    @if( $counter == 0 ) <?php continue; ?> @endif
+                                    @if( $counter++ == 0 ) <?php continue; ?> @endif
+                                        @if( $counter == 6 ) <?php break; ?> @endif
                                     <div class="col-sm-4 col-lg-3 col-md-4 col-xs-6">
                                         <div class="col-item">
                                             <div class="photo">
@@ -112,9 +112,58 @@
                             </div>
 
                         </div>
+                        <div class="col-sm-12 all_deal_box_wrap">
+                               <h1>More Recommendations</h1>
+                               <div class="col-sm-12 all_deal_box_wrap" style="margin-top:10px;">
+                                    <div class="row remove_clearfix">
+                                        <div class="col-sm-12"></div>
+                                    </div>
+                                    <?php $counter = 0; ?>
+                                    @foreach( $oDeals AS $oDeal )  
+                                       <!--  @if( $counter++ == 5 ) <?php continue; ?> @endif -->
+                                        @if( $counter <= 6 ) <?php continue; ?> @endif 
+                                    <div class="col-sm-4 col-lg-3 col-md-4 col-xs-6">
+                                <div class="col-item">
+                                    <div class="photo">
+                                        <a href="{{route('search.deals.show',[$oDeal->id])}}" style="margin-left:0;width:100%;">                                         
+                                            <?php /*<div class="user-image" align="center" style="height:260px;width:346px;position:relative;">                                                    
+                                                <?php
+                                                    $oDealsImages = $oDeal->DealImages();                                                        
+                                                    $image = ( $oDeal->DealImages->count() > 0 ) ? $oDeal->DealImages->first()->image_path : ( $oDealsImages->count()  ? $oDealsImages->first()->image_path : url('images/no_image.png'));
+                                                    $path =  ( $oDeal->DealImages->count() > 0 ) ? base_path('uploads/'.pathinfo($oDeal->DealImages->first()->image_path,PATHINFO_BASENAME)) : ( $oDealsImages->count()  ? base_path('uploads/'.pathinfo( $oDealsImages->first()->image_path,PATHINFO_BASENAME)) : base_path('images/no_image.png'));
+                                                    echo getImage($image,340,260,$path,true);
+
+                                            </div>*/ ?>
+                                            <?php
+                                                    $oDealsImages = $oDeal->DealImages(); 
+                                                    ?>
+                                            <img alt="{{$oDeal->title}}" style="height:185px;" class="img-responsive" src="{{Image::url(( $oDeal->DealImages->count() > 0 ) ? $oDeal->DealImages->first()->image_path : ( $oDealsImages->count()  ? $oDealsImages->first()->image_path : url('images/no_image.png')),350,185)}}">
+                                        </a>
+                                    </div>
+                                    <div class="info custom_info">
+                                        <div class="row" style="float:left; height:28px;">
+                                            <div class="price custom_price">
+                                                <a href="{{route('search.deals.show',[$oDeal->id])}}" style="width:100%;" align="left">
+                                                    <h5 style="text-indent: 0;font-size: 15px;">{{ ( strlen( $oDeal->title ) > 20 ) ? substr($oDeal->title,0,20)."..." : $oDeal->title }}</h5>
+                                                </a>
+                                            </div>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                        <div class="separator">
+                                            <p class="btn-add strike_through"><span style="float:left; text-transform:none;">{{ ( strlen( $oDeal->city ) > 30 ) ? substr($oDeal->city,0,30)."..." : $oDeal->city }}</span>${{$oDeal->original_price}}</p>
+                                            <p class="btn-details">${{$oDeal->new_price}} <!-- -{{$oDeal->discount_percentage}}% --></p>
+                                        </div>
+                                        <div class="clearfix">
+                                        </div>
+                                    </div>
+                                </div>
+
+                        </div>
+                        @endforeach
+                        
                         @endif
-
-
+ </div>
+                         </div>
                     </div>
                     @include('layouts.front_sidebar')
 
