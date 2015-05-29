41a267a9423fa5131b130f82f11ee9e1dc7f00a5
sagar rabadiya <sam.coolone70@gmail.com>
2015-05-29 14:41:29 +0530

    fixed 151

--- resources/views/emails/verification.blade.php
+++ resources/views/emails/verification.blade.php
@@ -2,14 +2,11 @@
     Hello {{ $user->firstname }} {{ $user->lastname }},<br><br>
     Welcome to tribal square<br><br>
     Your account information : <br><br>
-    Sign in Email : <br><br>
-    {{$user->email}}<br><br><br>
-
-    <h3 style="color:#76de10">Just one click and you're in.</h3><br>
-
-    click the link below to validate your email and you'll be automatically signed in to your new account. If you have completed all the steps of the registration process your profile will be active and searchable.
-    <br><br>
-    Use this link to validate your email Address: <br>
+    Sign in Email : {{$user->email}}<br>
+    <h3 style="color:#76de10">Just one click and you're in.</h3>
+    Click the link below to validate your email and you'll be automatically signed in to your new account. If you have completed all the steps of the registration process your profile will be active and searchable.
+    <br>
+    Use this link to validate your email address: <br>
     <a href="{{ action('Auth\RegisterController@getStep2')."?code=".$user->verfication_code  }}">Click here</a><br><br>
     We look forward to to help you.<br><br>
 
